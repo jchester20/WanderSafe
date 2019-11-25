a68119b414b07ed2eb81d78abd55b9533db2e722
Kristine Chen <krischen@berkeley.edu>
2019-11-24 17:01:31 -0800

    search screen 1

---
+++ app/src/main/java/com/example/wandersafe/SearchActivity.java
@@ -0,0 +1,24 @@
+package com.example.wandersafe;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        SearchView searchView = (SearchView) findViewById(R.id.searchview);
+        searchView.setIconified(false);
+        searchView.setIconifiedByDefault(false);
+
+        searchView.setQueryHint("City or neighborhood");
+
+    }
+}
