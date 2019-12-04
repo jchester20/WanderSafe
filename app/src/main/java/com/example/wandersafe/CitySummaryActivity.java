8143fc26274d4e1c8697395f6a7b67d1a643498c
Yalini Prabhakaran <>
2019-12-03 18:32:29 -0800

    finished summary screen try 1

--- app/src/main/java/com/example/wandersafe/CitySummaryActivity.java
+++ app/src/main/java/com/example/wandersafe/CitySummaryActivity.java
@@ -1,5 +1,6 @@
 package com.example.wandersafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,15 +10,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class CitySummaryActivity extends AppCompatActivity {
+
+    private Button elmwood_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_summary);
 
+        elmwood_btn = findViewById(R.id.button6);
 
+        elmwood_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(CitySummaryActivity.this, ReviewActivity.class);
+                CitySummaryActivity.this.startActivity(newIntent);
             }
+        });
+
+
+    }
 
 }
