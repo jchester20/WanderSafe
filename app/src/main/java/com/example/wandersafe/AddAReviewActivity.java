7355ce066380a3dd70cb6de1401e1c5d47920db0
Yalini Prabhakaran <>
2019-12-04 18:30:14 -0800

    made stars clickable

---
+++ app/src/main/java/com/example/wandersafe/AddAReviewActivity.java
@@ -0,0 +1,20 @@
+package com.example.wandersafe;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class AddAReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
