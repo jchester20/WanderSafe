09280f5d3d24db6de2453432e27e9c60b38a285c
Yalini Prabhakaran <>
2019-12-04 21:24:04 -0800

    Added closing

--- app/src/main/java/com/example/wandersafe/AddAReviewActivity.java
+++ app/src/main/java/com/example/wandersafe/AddAReviewActivity.java
@@ -1,5 +1,6 @@
 package com.example.wandersafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +10,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AddAReviewActivity extends AppCompatActivity {
+
+    private TextView close_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_areview);
+
+        close_btn = findViewById(R.id.close);
+        close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(AddAReviewActivity.this, ReviewActivity.class);
+                AddAReviewActivity.this.startActivity(newIntent);
+            }
+        });
+
     }
 
 }
