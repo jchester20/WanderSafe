530c1c079073c1df14c2cf4002e892520100da7b
Kristine Chen <krischen@berkeley.edu>
2019-12-04 21:04:31 -0800

    add setcontentview line

--- app/src/main/java/com/example/wandersafe/AddAReviewActivity.java
+++ app/src/main/java/com/example/wandersafe/AddAReviewActivity.java
@@ -15,6 +15,7 @@ public class AddAReviewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_areview);
     }
 
 }
